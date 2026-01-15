8392f5433429881c26af579af33166ad9b9260a6
edwineas <edwinalexshaji@gmail.com>
2026-01-15 21:06:33 +0530

    Add DbContext and configure SQLite connection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using FinanceTracker.Data;
 using FinanceTracker.Endpoints;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
 var app = builder.Build();
 
 app.MapHealthEndpoints();
